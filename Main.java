dc17535682e51a3bc1a3b7c0ef58496338f3aa16
A R <63975139+Magicallly@users.noreply.github.com>
2021-11-12 20:09:57 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Searcher searcher = new Searcher();
+
+        searcher.theCheapestApplianceOutput();
+        searcher.outputByName(Appliances.KETTLE.getApplianceNameToFile());
+    }
+}
